8efb75a4ab8b446a11174ae6bfda875f622a61b1
Beth Presten <bethpresten@gmail.com>
2021-01-30 20:25:20 -0500

    added starter code to package.json, added concurrently, axios, if-env

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  useEffect(() => {
+    axios.get("/api/config").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
