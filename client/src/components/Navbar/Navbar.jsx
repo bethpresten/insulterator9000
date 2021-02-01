68027b357be771315bed094e4a5754cec1fb16f3
Beth Presten <bethpresten@gmail.com>
2021-01-31 19:01:21 -0500

    targetted the Link directly inline to push a little to the right for visual appearance, commented a temporary image

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,12 +7,21 @@ function Navbar() {
     <div className="navbar-fixed">
       <nav>
         <div className="nav-wrapper">
-          <Link to="/" className="navbar-brand left">
+          <Link
+            to="/"
+            className="navbar-brand left"
+            style={{ marginLeft: "20px" }}
+          >
             {/* <img
-            src="https://previews.123rf.com/images/blamb/blamb1407/blamb140700837/29643265-a-cartoon-boy-sticks-out-his-tongue-as-an-insult-.jpg"
-            alt="boy with tongue out"
-            style={{ height: "50px", width: "50px", marginRight: "5px" }}
-          /> */}
+              src="https://previews.123rf.com/images/blamb/blamb1407/blamb140700837/29643265-a-cartoon-boy-sticks-out-his-tongue-as-an-insult-.jpg"
+              alt="boy with tongue out"
+              style={{
+                height: "50px",
+                width: "50px",
+                marginRight: "5px",
+                // marginTop: "5px",
+              }}
+            /> */}
             Insulterator9000
           </Link>
           <ul id="nav-mobile" className="right">
