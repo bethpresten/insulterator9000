a460bd02aff5b893bfa5fb40509dd37db20bd369
Beth Presten <bethpresten@gmail.com>
2021-02-02 14:53:53 -0500

    login page code added

--- client/src/containers/Login/Login.jsx
+++ client/src/containers/Login/Login.jsx
@@ -1,7 +1,56 @@
 import React from "react";
 
 function Login() {
-  return <div>This is the log in page</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>Sign in to be INSULERATED!</h1>
+      </div>
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6">
+              <i className="material-icons prefix">account_circle</i>
+              <input id="icon_prefix" type="text" className="validate" />
+              <label for="icon_prefix">First Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <i className="material-icons prefix">filter_vintage</i>
+              <input id="password" type="password" class="validate" />
+              <label for="password">Password</label>
+            </div>
+          </div>
+        </form>
+        <div class="row">
+          <div className="col s4">
+            <button
+              className="btn waves-effect waves-light hoverable"
+              type="submit"
+              name="action"
+            >
+              Sign in
+              <i class="material-icons right">send</i>
+            </button>
+          </div>
+          <div className="col s4">
+            {" "}
+            <h3> -OR- </h3>
+          </div>
+
+          <button
+            className="btn waves-effect waves-light hoverable"
+            type="submit"
+            name="action"
+          >
+            Register
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
