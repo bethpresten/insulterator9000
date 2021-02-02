350fd0e00c6176ee396b350460caac15779a4733
Justin Sykes <atlsykes1@att.net>
2021-02-02 14:42:37 -0500

    tweaked welcome.jsx to fix some errors and styling issue, tweaked styling on insultresults

--- client/src/containers/InsultResults/InsultResults.jsx
+++ client/src/containers/InsultResults/InsultResults.jsx
@@ -8,22 +8,29 @@ function InsultResults () {
       <br />
       <br />
       <br />
-      <div class='container'>
-        <div class='row'>
-        <div class='col s12 m12'>
-          <div class='card'>
-            <div class='card-content'>
-              <h3>{insult}</h3>
+      <div className='container'>
+        <div className='row'>
+          <div className='col s12 m12'>
+            <div className='card'>
+              <div className='card-content'>
+                <h3>{insult}</h3>
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className='row'>
+              <a className='waves-effect waves-light btn-large hoverable'>
+                Generate Another
+              </a>
+            </div>
+            <br />
+           
+            <div className='row'>
+              <a className='waves-effect waves-light btn-large hoverable'>
+                Go To Your Profile
+              </a>
             </div>
           </div>
-          <div class='row'>
-          <a class="waves-effect waves-light btn-large">Generate Another</a>
-            </div>
-            <br/>
-            <div class='row'>
-          <a class="waves-effect waves-light btn-large">Go To Your Profile</a>
-            </div>
-        </div>
         </div>
       </div>
     </div>
