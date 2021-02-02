4e4af9b9b2419e72b273430b6dae8b318b3e7311
Joseph Strickland <jks2090@outlook.com>
2021-02-01 21:08:25 -0500

    shit

--- client/src/containers/Welcome/Welcome.jsx
+++ client/src/containers/Welcome/Welcome.jsx
@@ -1,34 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function Welcome () {
-  return (
-    <div>
-      <br />
-      <div className='collection'>
-        <h1 id='welcome-title'>
-            WELCOME!! >>> GET YOUR SASSY #$%&#@* INSULTS HERE!
-        </h1>
-      </div>
-      <div>
-        <iframe
-          src='https://giphy.com/embed/qWpMdM0rGVgpW'
-          width='480'
-          height='264'
-          frameBorder='0'
-          class='giphy-embed'
-          allowFullScreen
-        ></iframe>
-        <iframe
-          src='https://giphy.com/embed/Pjg3K3U4y0tjbc17Mx'
-          width='480'
-          height='264'
-          frameBorder='0'
-          class='giphy-embed'
-          allowFullScreen
-        ></iframe>
-      </div>
-    </div>
-  )
+function Welcome() {
+  return <div>This is the welcome page</div>;
 }
 
-export default Welcome
+export default Welcome;
