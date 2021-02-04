b0e0f3a7588cc656f48a34461ba0b5c4c70849e0
Beth Presten <bethpresten@gmail.com>
2021-02-03 21:19:17 -0500

    added component for the update profile form, added card, form, and buttons

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Contact from "./containers/Contact/Contact";
 import Registration from "./containers/Registration/Registration";
 import Login from "./containers/Login/Login";
 import Dashboard from "./containers/Dashboard/Dashboard";
+import UpdateProfile from "./containers/UpdateProfile/UpdateProfile";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import { useEffect } from 'react'
@@ -18,7 +19,7 @@ filter.addWords('dicks', 'fuckton')
 function App() {
   useEffect(() => {
 
-//Get insult by user route    
+    //Get insult by user route    
     // axios
     //   .get(
     //     `api/insult/getbyuser/name`,
@@ -46,8 +47,9 @@ function App() {
             <Route path="/dashboard" component={Dashboard} />
             <Route path="/registration" component={Registration} />
             <Route path="/insultresults" component={InsultResults} />
+            <Route path="/updateprofile" component={UpdateProfile} />
           </Switch>
-        <Footer />
+          <Footer />
         </Router>
       </div>
     </>
--- client/src/containers/UpdateProfile/UpdateProfile.jsx
+++ client/src/containers/UpdateProfile/UpdateProfile.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import axios from "axios";
+import ProductForm from "../../components/UpdateProfileForm/UpdateProfileForm";
 
-function UpdateProfile() {
-  return <div>This is the update profile page</div>;
-}
+const UpdateProfile = () => {
+  const handleFormSubmit = (e, userData) => {
+    e.preventDefault();
+    axios
+      .post("/api/user", userData)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col s3">
+          <div className="card horizontal hoverable">
+            <div className="card-stacked">
+              <div className="card-content">
+                <h5>Name</h5>
+              </div>
+              <div className="card-content">
+                <h5>Hobbies</h5>
+              </div>
+              <div className="card-content">
+                <h5>Favorite Sports Teams</h5>
+              </div>
+              <div className="card-content">
+                <h5>Occupation</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col sm9">
+          <ProductForm handleFormSubmit={handleFormSubmit} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default UpdateProfile;
