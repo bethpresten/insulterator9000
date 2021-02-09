7a48bd27f937348e05db3cfde2b02d158ca2ea74
Justin Sykes <atlsykes1@att.net>
2021-02-08 20:02:57 -0500

    working on insultsresults logic and committing for pull

--- client/src/containers/InsultResults/InsultResults.jsx
+++ client/src/containers/InsultResults/InsultResults.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './InsultResults.css'
+import axios from 'axios'
 const insult = 'Why you stuck-up, half-witted, scruffy-looking nerfherder!'
 
 function InsultResults () {
   return (
+
     <div>
       <br />
       <br />
@@ -37,3 +39,67 @@ function InsultResults () {
 }
 
 export default InsultResults
+
+getInsult () => {
+  //generate a random number 1 - 6
+  return Math.floor(Math.random() * Math.floor(max));
+  // select one of the following templates based on number
+  axios
+  .get(`https://insult.mattbas.org/api/insult.json?template=The+${sportsTeam}+are+as+%3Cadjective%3E+as+%3Carticle+target%3Dadj1%3E+%3Cadjective+min%3D1+max%3D3+id%3Dadj1%3E+%3Camount%3E+of+%3Cadjective+min%3D1+max%3D3%3E+%3Canimal%3E+%3Canimal_part%3E`)
+  .then(function (response) {
+    console.log(`before: ${response.data.insult}`)
+    console.log(`after: ${filter.clean(response.data.insult)}`)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+axios
+  .get(`https://insult.mattbas.org/api/insult.json?template=Only+${sportsTeam}+fans+are+%3Cadjective%3E+%3Canimal%3E+%3Canimal_part%3E`)
+  .then(function (response) {
+    console.log(`before: ${response.data.insult}`)
+    console.log(`after: ${filter.clean(response.data.insult)}`)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+
+//occupation insults
+axios
+  .get(`https://insult.mattbas.org/api/insult.json?template=People+who+are+${jobTitle}s+secretly+eat+%3Canimal%3E+%3Canimal_part%3E`)
+  .then(function (response) {
+    console.log(`before: ${response.data.insult}`)
+    console.log(`after: ${filter.clean(response.data.insult)}`)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+axios
+  .get(`https://insult.mattbas.org/api/insult.json?template=So+you%27re+a+${jobTitle}%3F+I+didn%27t+know+you+were+%3Carticle+target%3Dadj1%3E+%3Cadjective+id%3Dadj1%3E+%3Camount%3E+of+%3Canimal%3E+%3Canimal_part%3E`)
+  .then(function (response) {
+    console.log(`before: ${response.data.insult}`)
+    console.log(`after: ${filter.clean(response.data.insult)}`)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+
+//hobby insults
+axios
+  .get(`https://insult.mattbas.org/api/insult.json?template=People+who+like+${hobbyType}+really+like+%3Canimal%3E+%3Canimal_part%3E+because+they%27re+%3Cadjective%3E`)
+  .then(function (response) {
+    console.log(`before: ${response.data.insult}`)
+    console.log(`after: ${filter.clean(response.data.insult)}`)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+axios
+  .get(`https://insult.mattbas.org/api/insult.json?template=You+are+%3Cadjective%3E+if+you+like+${hobbyType}%2C+you+%3Cadjective%3E+%3Canimal%3E`)
+  .then(function (response) {
+    console.log(`before: ${response.data.insult}`)
+    console.log(`after: ${filter.clean(response.data.insult)}`)
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
+}
