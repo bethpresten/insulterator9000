b0febdeba6dda9b743d3cfbde933e2e0316f76db
Justin Sykes <atlsykes1@att.net>
2021-02-10 20:55:12 -0500

    updated usercard and registration page to handle user initialization and localStorage being null

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -1,25 +1,32 @@
 import React, { useContext, useState, useEffect } from 'react'
 import axios from 'axios'
-import './UserCard.css'
-import globalUser from '../../utils/globalUser'
-const email = JSON.parse(localStorage.getItem('user')).email
-console.log(email)
-
-// this will come from user registration
+const initialState = {
+  firstname: '',
+  lastname: '',
+  hobby: '',
+  sport: '',
+  occupation: ''
+}
 
 const UserCard = () => {
-  const [data, setData] = useState('')
+  const [data, setData] = useState(initialState)
 
   const getData = () => {
-    axios
-      .get(`/api/users/get-user/${email}`)
-      .then(response => {
-        console.log(response.data)
-        setData(response.data)
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    const id = localStorage.getItem('user')
+    console.log(id)
+    if (id) {
+      axios
+        .get(`/api/users/get-user/${id}`)
+        .then(response => {
+          console.log(response.data)
+          if (response.data) {
+            setData(response.data)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
   }
 
   useEffect(() => {
@@ -35,7 +42,7 @@ const UserCard = () => {
               Name: {data.firstname} {data.lastname}
             </h5>
             <h5>Hobby: {data.hobby}</h5>
-            <h5>Favorite Sports Team: {data.sport}</h5>
+            <h5>Favorite Sport: {data.sport}</h5>
             <h5>Occupation: {data.occupation}</h5>
           </div>
         </div>
