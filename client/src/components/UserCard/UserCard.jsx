9e5dbc4dd1075ee489156162754b04c784527ed0
Beth Presten <bethpresten@gmail.com>
2021-02-10 19:26:14 -0500

    bad merge

--- client/src/components/UserCard/UserCard.jsx
+++ client/src/components/UserCard/UserCard.jsx
@@ -1,20 +1,47 @@
 import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./UserCard.css";
 import globalUser from "../../utils/globalUser";
-const email = "billybob@billy.com";
+// const email = "billybob@billy.com";
 
 // this will come from user registration
 
 const UserCard = () => {
   const [data, setData] = useState("");
+  const [firstname, setFirstName] = useState("");
+  const [lastname, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [occupation, setOccupation] = useState("");
+  const [sport, setSport] = useState("");
+  const [hobby, setHobby] = useState("");
 
+  const { id } = useParams();
   const getData = () => {
     axios
-      .get(`/api/users/get-user/${email}`)
+      .get(`/api/users/get-user/${id}`)
       .then((response) => {
         console.log(response.data);
-        setData(response.data);
+        const {
+          firstname,
+          lastname,
+          email,
+          password,
+          occupation,
+          sport,
+          hobby,
+        } = response.data;
+        setFirstName(firstname);
+        setLastName(lastname);
+        setEmail(email);
+        setPassword(password);
+        setOccupation(occupation);
+        setSport(sport);
+        setHobby(hobby);
+      })
+      .catch((err) => {
+        console.log(err);
       })
       .catch((err) => {
         console.log(err);
@@ -25,7 +52,7 @@ const UserCard = () => {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [id]);
 
   return (
     <div className>
@@ -33,11 +60,11 @@ const UserCard = () => {
         <div className="card-stacked">
           <div className="card-content left-align">
             <h5>
-              Name: {newUser.firstname} {newUser.lastname}
+              Name: {firstname} {lastname}
             </h5>
-            <h5>Hobby: {newUser.hobby}</h5>
-            <h5>Favorite Sports Team: {newUser.sport}</h5>
-            <h5>Occupation: {newUser.occupation}</h5>
+            <h5>Hobby: {hobby}</h5>
+            <h5>Favorite Sports Team: {sport}</h5>
+            <h5>Occupation: {occupation}</h5>
           </div>
         </div>
       </div>
