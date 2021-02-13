7ec748b33160cd822cfbdd2f56c6ee2d10b3fb62
Justin Sykes <atlsykes1@att.net>
2021-02-13 09:39:09 -0500

    updated insults results to be fully dynamic

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,8 +6,8 @@ const Filter = require('bad-words'),
   filter = new Filter()
 filter.addWords('dicks', 'fuckton', 'fuckload', 'assload')
 let newURL = ''
-
 let randNumb = 0
+let userData = {}
 
 //get all users
 router.get('/', (req, res) => {
@@ -66,7 +66,7 @@ router.delete('/delete-user/:id', (req, res) => {
 
 // get insult from 3rd party API route
 
-router.get('/get-insult', (req, res) => {
+router.get('/get-insult/', (req, res) => {
   newURL = selectTemplate()
   axios
     .get(newURL)
@@ -80,26 +80,37 @@ router.get('/get-insult', (req, res) => {
     })
 })
 
+// route to all passing user data (id, sport, occupation, hobby) from FE to BE
+
+router.post('/data', (req, res) => {
+  console.log(req.body)
+  userData = req.body
+  console.log(userData.sport)
+  console.log(userData.hobby)
+  console.log(userData.occupation)
+})
+
 //start insult templates
 const selectTemplate = insultURL => {
+  console.log(userData)
   randNumb = Math.floor(Math.random() * Math.floor(5))
   console.log(`Template #${randNumb} selected!`)
   switch (randNumb) {
     //Sports Team insults
     case 0:
-      return `https://insult.mattbas.org/api/insult.json?template=The+${sport}+are+as+%3Cadjective%3E+as+%3Carticle+target%3Dadj1%3E+%3Cadjective+min%3D1+max%3D3+id%3Dadj1%3E+%3Camount%3E+of+%3Cadjective+min%3D1+max%3D3%3E+%3Canimal%3E+%3Canimal_part%3E`
+      return `https://insult.mattbas.org/api/insult.json?template=The+${userData.sport}+are+as+%3Cadjective%3E+as+%3Carticle+target%3Dadj1%3E+%3Cadjective+min%3D1+max%3D3+id%3Dadj1%3E+%3Camount%3E+of+%3Cadjective+min%3D1+max%3D3%3E+%3Canimal%3E+%3Canimal_part%3E`
     case 1:
-      return `https://insult.mattbas.org/api/insult.json?template=Only+${sport}+fans+are+%3Cadjective%3E+%3Canimal%3E+%3Canimal_part%3E`
+      return `https://insult.mattbas.org/api/insult.json?template=Only+${userData.sport}+fans+are+%3Cadjective%3E+%3Canimal%3E+%3Canimal_part%3E`
     //Occupation insults
     case 2:
-      return `https://insult.mattbas.org/api/insult.json?template=People+who+are+${occupation}s+secretly+eat+%3Canimal%3E+%3Canimal_part%3E`
+      return `https://insult.mattbas.org/api/insult.json?template=People+who+are+${userData.occupation}s+secretly+eat+%3Canimal%3E+%3Canimal_part%3E`
     case 3:
-      return `https://insult.mattbas.org/api/insult.json?template=So+you%27re+a+${occupation}%3F+I+didn%27t+know+you+were+%3Carticle+target%3Dadj1%3E+%3Cadjective+id%3Dadj1%3E+%3Camount%3E+of+%3Canimal%3E+%3Canimal_part%3E`
+      return `https://insult.mattbas.org/api/insult.json?template=So+you%27re+a+${userData.occupation}%3F+I+didn%27t+know+you+were+%3Carticle+target%3Dadj1%3E+%3Cadjective+id%3Dadj1%3E+%3Camount%3E+of+%3Canimal%3E+%3Canimal_part%3E`
     //hobby insults
     case 4:
-      return `https://insult.mattbas.org/api/insult.json?template=People+who+like+${hobby}+really+like+%3Canimal%3E+%3Canimal_part%3E+because+they%27re+%3Cadjective%3E`
+      return `https://insult.mattbas.org/api/insult.json?template=People+who+like+${userData.hobby}+really+like+%3Canimal%3E+%3Canimal_part%3E+because+they%27re+%3Cadjective%3E`
     default:
-      return `https://insult.mattbas.org/api/insult.json?template=You+are+%3Cadjective%3E+if+you+like+${hobby}%2C+you+%3Cadjective%3E+%3Canimal%3E`
+      return `https://insult.mattbas.org/api/insult.json?template=You+are+%3Cadjective%3E+if+you+like+${userData.hobby}%2C+you+%3Cadjective%3E+%3Canimal%3E`
   }
 }
 
