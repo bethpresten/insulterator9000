1576533d4f4772cae13cbcff8f2f5745ab118030
Justin Sykes <atlsykes1@att.net>
2021-02-15 08:58:59 -0500

    updated welcome.jsx to add login and registration buttons, tweaked Navbar, updated contact.css

--- client/src/containers/Welcome/Welcome.jsx
+++ client/src/containers/Welcome/Welcome.jsx
@@ -1,48 +1,48 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Welcome.css";
-function Welcome() {
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Welcome.css'
+function Welcome () {
   return (
     <div>
       <br />
-      <div className="collection">
-        <h1 id="welcome-title">
-          WELCOME!! >>> GET YOUR SASSY #$%&#@* INSULTS HERE!
+      <div className='collection'>
+        <h1 id='welcome-title'>
+          WELCOME!! >> GET YOUR SASSY #$%&#@* INSULTS HERE!
         </h1>
       </div>
       <div>
         <iframe
-          src="https://giphy.com/embed/qWpMdM0rGVgpW"
-          width="480"
-          height="264"
-          frameBorder="0"
-          className="giphy-embed left"
-          title="Anchorman insult"
+          src='https://giphy.com/embed/qWpMdM0rGVgpW'
+          width='480'
+          height='264'
+          frameBorder='0'
+          className='giphy-embed left'
+          title='Anchorman insult'
           allowFullScreen
         ></iframe>
-        <Link className="btn-large hoverable" to="/login" id="login-button">
+        <Link className='btn-large hoverable' to='/login' id='login-button'>
           Login
         </Link>
         <Link
-          className="btn-large hoverable"
-          to="/registration"
-          id="register-button"
+          className='btn-large hoverable'
+          to='/registration'
+          id='register-button'
         >
           Register
         </Link>
         <iframe
-          src="https://giphy.com/embed/Pjg3K3U4y0tjbc17Mx"
-          width="480"
-          height="264"
-          frameBorder="0"
-          className="giphy-embed right"
-          title="Ricky Gervais insult"
+          src='https://giphy.com/embed/Pjg3K3U4y0tjbc17Mx'
+          width='480'
+          height='264'
+          frameBorder='0'
+          className='giphy-embed right'
+          title='Ricky Gervais insult'
           allowFullScreen
         ></iframe>
         <br />
         <br />
       </div>
     </div>
-  );
+  )
 }
-export default Welcome;
+export default Welcome
